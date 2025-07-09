dbd9a766ba892d18d690cb6a67c3cf0fbdbb281a
Wambu-i7 <wambuindikiru@gmail.com>
2025-07-09 16:35:36 +0300

    nav bar responsiveness updated

--- script.js
+++ script.js
@@ -4,3 +4,14 @@
     navToggle.addEventListener('click', () => {
         navMenu.classList.toggle('show');
     });
+
+    // Close the nav when clicking anywhere outside the nav
+    document.addEventListener("click", (event) => {
+    const isClickInsideNav = navMenu.contains(event.target);
+    const isClickOnToggle = navToggle.contains(event.target);
+
+    // If click is outside both nav and toggle button, hide the menu
+    if (!isClickInsideNav && !isClickOnToggle) {
+        navMenu.classList.remove("show");
+    }
+    });
